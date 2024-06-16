51eef23d8ebf57f0199951dd2f65100ba8df0db1
Infinity2326 <dima.gorbachev2002@mail.ru>
2024-06-16 12:22:37 +0300

    fix: register page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -51,23 +51,23 @@ export const RegisterPage = () => {
     >
       <h1 className="text-lg text-white text-center">Регистрация</h1>
       <label className="text-xs text-gray-400">
-        Username:
+        Имя пользователя:
         <input
           type="text"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
+          placeholder="Логин"
           className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
         />
       </label>
 
       <label className="text-xs text-gray-400">
-        Password:
+        Пароль:
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
+          placeholder="Введите пароль"
           className="mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700"
         />
       </label>
